12a09b0495129e6b87c55e90de1ee18f87e88402
Jhonny <jhibag97@gmail.com>
2020-05-06 20:28:34 +0200

    Main page and language page commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'languages',
+    loadChildren: () => import('./languages/languages.module').then( m => m.LanguagesPageModule)
+  },
 ];
 
 @NgModule({
